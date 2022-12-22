33fb8bd2c1d3f59fafca532bd38b973c977e4bb5
Thomas Brodusch <brodusch.thomas@gmail.com>
2022-12-22 13:33:53 +0100

    chore: src/main.js -> src/index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as transformers from "@/transformers";
+import * as sanitizers from "@/sanitizers";
+import * as checkers from "@/checkers";
+
+export default { ...transformers, ...sanitizers, ...checkers };
