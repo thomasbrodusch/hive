68c6465d5a9e22129eff27b8bb87066c80c528a2
Thomas Brodusch <brodusch.thomas@gmail.com>
2022-12-30 08:41:37 +0100

    build: add vitest-fail-on-console dependency

--- tests/support/setup.ts
+++ tests/support/setup.ts
@@ -1,14 +1,2 @@
-import { SpyInstance, vi } from "vitest";
-
-const spyError = vi.spyOn(console, "error");
-const spyWarn = vi.spyOn(console, "warn");
-
-afterEach(() => {
-  const throwIfFailure = (spy: SpyInstance) => {
-    if (spy.mock.calls.length) {
-      throw new Error(`Expected test not to call console.${spy.getMockName()}`);
-    }
-  };
-  throwIfFailure(spyError);
-  throwIfFailure(spyWarn);
-});
+import failOnConsole from "vitest-fail-on-console";
+failOnConsole();
